a1815a8a1dbae129b281330691e4ef64c8c0f46d
Emil Enev <emil.enev@abv.bg>
2021-09-19 15:48:52 +0300

    feat: add hot icon

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  const hot = document.querySelectorAll(".hot")
+
+
+  for (let i = 0; i < hot.length; i++) {
+    hot[i].innerHTML += "🔥"
+    console.log(hot[i].innerHTML)
+  }
 });
